6fc534074ac80f9c2aa733af2ac2b7c4b56b6b4f
Marcos Sierra <marcosjspac@gmail.com>
2023-02-11 20:30:05 -0600

    Agregando microservicio principal

---
+++ discovery-service-principal/src/test/java/org/marcos/discoveryserviceprincipal/DiscoveryServicePrincipalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.marcos.discoveryserviceprincipal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServicePrincipalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
